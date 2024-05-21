7e7c791261caa4b6d3238d277bbb13b67338ab14
Anil <halfdeadentity@gmail.com>
2024-05-21 11:32:05 +0530

    pushing to github

--- src/app/v/user/route.js
+++ src/app/v/user/route.js
@@ -1,3 +1,16 @@
+import { NextResponse } from "next/server";
+
 const { default: dbConnect } = require("@/config/dbConnect");
 
 dbConnect();
+
+
+export const GET = () => {
+    try {
+        return NextResponse.json({ message: "god" });
+    } catch (error) {
+        return NextResponse.json({
+            message: "error"
+        }).body(error);
+    }
+}
